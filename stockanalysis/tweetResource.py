95713b346f78084b244b2d85f834d92042e37ac0
Rajiv Annapragada <rajiv.annapragada@gmail.com>
2015-04-17 03:29:20 -0700

    added cors

--- stockanalysis/tweetResource.py
+++ stockanalysis/tweetResource.py
@@ -3,10 +3,21 @@ Created on Apr 16, 2015
 
 @author: rajiv
 '''
-from bottle import run, route, response
+from bottle import run, route, response, hook
 import tweetRepository
 
 mySymbols = {"$AAPL"}
+
+@hook('after_request')
+def enable_cors():
+    """
+    You need to add some headers to each request.
+    Don't use the wildcard '*' for Access-Control-Allow-Origin in production.
+    """
+    response.headers['Access-Control-Allow-Origin'] = '*'
+    response.headers['Access-Control-Allow-Methods'] = 'PUT, GET, POST, DELETE, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Origin, Accept, Content-Type, X-Requested-With, X-CSRF-Token'
+
 
 @route('/getTopTweetsbySymbol/<sym>')
 def getTopTweetsbySymbol(sym):
