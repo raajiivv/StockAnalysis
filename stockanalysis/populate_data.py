12d7dc15a6c5c0ed852e1dea08148fd50fe4dacc
Rajiv Annapragada <rajiv.annapragada@gmail.com>
2015-04-26 17:41:41 -0700

    changed the stock trends; added updates on weekend

--- stockanalysis/populate_data.py
+++ stockanalysis/populate_data.py
@@ -4,12 +4,11 @@ Created on Apr 19, 2015
 @author: rajiv
 '''
 
-#from tweetRepository import delete_tweets, process_tweets, store_tweets,store_sentiment
 import tweetRepository as tr
 import datetime, time, requests
 
 def populate():
-    date = int(datetime.date.today().strftime("%d"))
+    global date
     while True:
         h = datetime.datetime.utcnow().hour
         w = datetime.datetime.today().weekday()
@@ -17,37 +16,41 @@ def populate():
 
         # Does not populate on weekends
         if w == 5 or w == 6:
+            if h == 12 or h == 23:
+                populate_db(d, 1000)
+                time.sleep(3600)
             continue
         # Does not populate after working hours
         if h< 13 or h> 20:
             continue
         
-        if d!=date:
-            tr.delete_sentiment_count()
-            
-            print date, d
-            date = d
-        
-        for symbol in mySymbols:
-            tr.process_tweets(symbol, 100)
-        tr.delete_tweets()
-        tr.store_tweets()
-        print datetime.datetime.now().time()
-        
-        for symbol in mySymbols:
-            tr.store_sentiment(symbol)        
-            tr.store_sentiment_count(symbol)
-            tr.store_top_tweets(symbol)
-            
+        populate_db(d, 100)
         # Waits for 15 minutes before populating the data again
-        time.sleep(900)        
+        time.sleep(900)
+
+def populate_db(d, count):
+    global date
+    if d!=date:
+        tr.delete_sentiment_count()
+        print date, d, count
+        date = d
+    
+    for symbol in mySymbols:
+        tr.process_tweets(symbol, count)
+    tr.delete_tweets()
+    tr.store_tweets()
+    print datetime.datetime.now().time()
+    
+    for symbol in mySymbols:
+        tr.store_sentiment(symbol)        
+        tr.store_sentiment_count(symbol)
+        tr.store_top_tweets(symbol)
+        
             
 mySymbols = set()
 results = requests.get(url="http://54.191.103.141:8800/getSymbols")
-print results
 response = results.json()
 for symbol in response:
     mySymbols.add(symbol['symbol'])
-
-#mySymbols = {"$AAPL", "$BAC", "$GE", "$CMCSA", "$MSFT", "$CSCO", "$F", "$INTC", "$T", "$PFE"}
+date = int(datetime.date.today().strftime("%d"))
 populate()
